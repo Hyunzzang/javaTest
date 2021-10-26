f465339b0571436a7e08222cdf234a633681d1ba
박현철 <Speech@tmon.co.kr>
2021-10-26 12:15:47 +0900

    ..

--- src/test/java/com/zzang/test/BaseTest.java
+++ src/test/java/com/zzang/test/BaseTest.java
@@ -17,4 +17,15 @@ public class BaseTest {
 
         System.out.println("OK");
     }
+
+    @Test
+    public void stringTest() {
+        String str = "abcdef";
+
+        System.out.println("charAt : " + str.charAt(1));
+        System.out.println("toCharArray : " + str.toCharArray());
+        System.out.println("indexOf : " + str.indexOf('a'));
+        System.out.println("indexOf : " + str.indexOf("bc"));
+        System.out.println("indexOf : " + str.indexOf("bc", 1));
+    }
 }
