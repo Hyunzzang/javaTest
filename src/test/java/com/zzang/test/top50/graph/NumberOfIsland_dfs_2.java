45d561572d2e302a1113da00ef3825db18982a97
Hyunzzang <phc023@gmail.com>
2021-11-15 20:19:54 +0900

    ..

---
+++ src/test/java/com/zzang/test/top50/graph/NumberOfIsland_dfs_2.java
@@ -0,0 +1,79 @@
+package com.zzang.test.top50.graph;
+
+public class NumberOfIsland_dfs_2 {
+
+  public static void main(String[] args) {
+    char[][] grid = {
+        {'1', '1', '0', '0', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'0', '0', '0', '1', '1'}};
+
+//		char[][] grid = {
+//				{ '1', '0' },
+//				{ '1', '1'}};
+
+    NumberOfIsland_dfs_2 a = new NumberOfIsland_dfs_2();
+    System.out.println(a.solve(grid));
+  }
+
+  int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+  int m, n;
+  int dfsCount = 0;
+
+  public int solve(char[][] grid) {
+    if (grid == null || grid.length == 0 || grid[0].length == 0) {
+      return 0;
+    }
+    m = grid.length;
+    n = grid[0].length;
+    int count = 0;
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == '1') {
+          System.out.println("======================i " + i + " j " + j);
+          count++;
+          dfs(grid, i, j);
+        }
+      }
+    }
+    print(grid);
+    return count;
+  }
+
+  public void dfs(char[][] grid, int i, int j) {
+    dfsCount++;
+    System.out.println("===i " + i + " j " + j + " dfsCount " + dfsCount);
+    print(grid);
+    // 1.error
+//		if(i<0 || j<0 || i >= m || j >= n || grid[i][j]!='1')
+//			return;
+    if (i >= 0 && j >= 0 && i < m && j < n && grid[i][j] == '1') {
+
+      grid[i][j] = 'X';
+      System.out.println("grid[" + i + "][" + j + "] " + grid[i][j]);
+      for (int[] dir : dirs) {
+        dfs(grid, i + dir[0], j + dir[1]);
+      }
+      System.out.println("====end====" + " dfsCount " + dfsCount);
+    }
+
+//		dfs(grid, i-1,j);
+//		dfs(grid, i+1,j);
+//		dfs(grid, i,j-1);
+//		dfs(grid, i,j+1);
+  }
+
+  public void print(char[][] grid) {
+    for (int i = 0; i < grid.length; i++) {
+//			System.out.print("i "+i);
+      for (int j = 0; j < grid[i].length; j++) {
+//				System.out.println(grid[i]);
+        System.out.print("[" + i + "][" + j + "]" + grid[i][j]);
+//				System.out.print(grid[i][j]);
+      }
+      System.out.println();
+    }
+  }
+}
--- src/test/java/com/zzang/test/top50/graph/RemoveInvalidParentheses.java
+++ src/test/java/com/zzang/test/top50/graph/RemoveInvalidParentheses.java
@@ -8,37 +8,44 @@ import java.util.Queue;
 import java.util.Set;
 
 public class RemoveInvalidParentheses {
+
   public static void main(String[] args) {
     String s = "(a)())()";
     RemoveInvalidParentheses a = new RemoveInvalidParentheses();
     System.out.println(a.solve(s));
   }
-  public List<String> solve(String s){
+
+  public List<String> solve(String s) {
     List<String> result = new ArrayList<>();
-    if(s==null)
+    if (s == null) {
       return result;
+    }
     Queue<String> queue = new LinkedList<>();
     Set<String> visited = new HashSet<>();
     queue.offer(s);
     visited.add(s);
-    boolean found =false;
+    boolean found = false;
 
-    while(!queue.isEmpty()) {
+    while (!queue.isEmpty()) {
       int size = queue.size();
-      for(int i=0; i<size; i++) {
+      for (int i = 0; i < size; i++) {
         String str = queue.poll();
-        if(isValid(str)) {
+        if (isValid(str)) {
           result.add(str);
           found = true;
-          System.out.println("str: "+str+" found "+found);
+          System.out.println("str: " + str + " found " + found);
         }
-        System.out.println("found "+found);
-        if(found) continue;
-        for(int j=0; j<str.length(); j++) {
-          if(str.charAt(j)!= '(' && str.charAt(j) !=')') continue;
-          System.out.println(str.substring(0,j)+":"+str.substring(j+1));
-          String newStr = str.substring(0,j)+str.substring(j+1);
-          if(!visited.contains(newStr)) {
+        System.out.println("found " + found);
+        if (found) {
+          continue;
+        }
+        for (int j = 0; j < str.length(); j++) {
+          if (str.charAt(j) != '(' && str.charAt(j) != ')') {
+            continue;
+          }
+          System.out.println(str.substring(0, j) + ":" + str.substring(j + 1));
+          String newStr = str.substring(0, j) + str.substring(j + 1);
+          if (!visited.contains(newStr)) {
 //						System.out.println("newStr: "+newStr);
             queue.offer(newStr);
             visited.add(newStr);
@@ -49,17 +56,20 @@ public class RemoveInvalidParentheses {
     }
     return result;
   }
-  private boolean isValid(String str) {
-    int count=0;
 
-    for(char c : str.toCharArray()) {
-      if(c=='(') {
+  private boolean isValid(String str) {
+    int count = 0;
+
+    for (char c : str.toCharArray()) {
+      if (c == '(') {
         count++;
-      }else if(c==')') {
+      } else if (c == ')') {
         count--;
-        if(count<0) return false;
+        if (count < 0) {
+          return false;
+        }
       }
     }
-    return count==0;
+    return count == 0;
   }
 }
