4510b71216bfa0bd2d350b9e580d59001590f234
Hyunzzang <phc023@gmail.com>
2022-03-24 21:39:02 +0900

    .

--- src/test/java/com/zzang/test/top50/backTracking/LetterCombi.java
+++ src/test/java/com/zzang/test/top50/backTracking/LetterCombi.java
@@ -3,33 +3,45 @@ package com.zzang.test.top50.backTracking;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that
+ * the number could represent. Return the answer in any order.
+ * <p>
+ * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1
+ * does not map to any letters.
+ */
 public class LetterCombi {
-  public static void main(String[] args) {
-    LetterCombi a = new LetterCombi();
-    a.solve("23");
-  }
 
-  public List<String> solve(String digits) {
-    String digitletter[] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-    List<String> result = new ArrayList<String>();
+    public static void main(String[] args) {
+        LetterCombi a = new LetterCombi();
+        a.solve("23");
+    }
 
-    if (digits.length() == 0)
-      return result;
+    public List<String> solve(String digits) {
+        String digitletter[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> result = new ArrayList<String>();
 
-    result.add("");
-    for (int i = 0; i < digits.length(); i++)
-      result = combine(digitletter[digits.charAt(i) - '0'], result);
+        if (digits.length() == 0) {
+            return result;
+        }
 
-    return result;
-  }
+        result.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            result = combine(digitletter[digits.charAt(i) - '0'], result);
+        }
 
-  public List<String> combine(String digit, List<String> firstList) {
-    List<String> result = new ArrayList<String>();
+        return result;
+    }
 
-    for (int i = 0; i < digit.length(); i++)
-      for (String firstStr : firstList)
-        result.add(firstStr + digit.charAt(i));
+    public List<String> combine(String digit, List<String> firstList) {
+        List<String> result = new ArrayList<String>();
 
-    return result;
-  }
+        for (int i = 0; i < digit.length(); i++) {
+            for (String firstStr : firstList) {
+                result.add(firstStr + digit.charAt(i));
+            }
+        }
+
+        return result;
+    }
 }
--- src/test/java/com/zzang/test/top50/backTracking/Permutation.java
+++ src/test/java/com/zzang/test/top50/backTracking/Permutation.java
@@ -3,53 +3,58 @@ package com.zzang.test.top50.backTracking;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given an array nums of distinct integers, return all the possible permutations. You can return
+ * the answer in any order.
+ */
 public class Permutation {
 
-  public static void main(String[] args) {
-    Permutation a = new Permutation();
-    int[] nums = {1, 2, 3};
+    public static void main(String[] args) {
+        Permutation a = new Permutation();
+        int[] nums = {1, 2, 3};
 //		System.out.println(a.permute_backTrack(nums));
-    System.out.println(a.permute_dfs(nums));
-  }
-
-  public List<List<Integer>> permute_dfs(int[] nums) {
-    List<List<Integer>> lists = new ArrayList<>();
-    if (nums == null || nums.length == 0) {
-      return lists;
+        System.out.println(a.permute_dfs(nums));
     }
 
-    dfs(nums, lists, new ArrayList<Integer>());
-    return lists;
-  }
+    public List<List<Integer>> permute_dfs(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return lists;
+        }
 
-  int count = 0;
-
-  private void dfs(int[] nums, List<List<Integer>> lists, List<Integer> cur) {
-    count++;
-    System.out.println("--start--" + cur);
-
-    if (cur.size() == nums.length) {
-      List<Integer> list = new ArrayList<>(cur);
-      lists.add(list);
+        dfs(nums, lists, new ArrayList<Integer>());
+        return lists;
     }
 
-    for (int i = 0; i < nums.length; i++) {
-      if (cur.contains(nums[i])) {
-        continue;
-      }
+    int count = 0;
+
+    private void dfs(int[] nums, List<List<Integer>> lists, List<Integer> cur) {
+        count++;
+        System.out.println("--start--" + cur);
+
+        if (cur.size() == nums.length) {
+            List<Integer> list = new ArrayList<>(cur);
+            lists.add(list);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (cur.contains(nums[i])) {
+                continue;
+            }
 //	        if (cur.size() == nums.length) {
 //        	continue;
 //	    }
-      System.out.println(
-          "========i: " + i + " nums[" + i + "]=" + nums[i] + " cur: " + cur + " count " + count);
-      cur.add(nums[i]);
+            System.out.println(
+                "========i: " + i + " nums[" + i + "]=" + nums[i] + " cur: " + cur + " count "
+                    + count);
+            cur.add(nums[i]);
 //	        System.out.println("########i: "+i+" nums[" + i + "]=" + nums[i]+" cur: "+cur+" count "+count);
-      dfs(nums, lists, cur);
-      System.out.println("size = " + cur.size());
-      cur.remove(cur.size() - 1);
-    }
+            dfs(nums, lists, cur);
+            System.out.println("size = " + cur.size());
+            cur.remove(cur.size() - 1);
+        }
 
-    System.out.println("--end--" + cur);
-    System.out.println();
-  }
+        System.out.println("--end--" + cur);
+        System.out.println();
+    }
 }
